12eb4d6fd8f9f1d10e3f9b267d341ee9d3c7288d
TU7SHAR <brahmin00011@gmail.com>
2026-02-19 14:29:40 +0530

    boilerplate updated

---
+++ src/boilerplate.js
@@ -0,0 +1,44 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { Pane } from "tweakpane";
+import "./style.css";
+
+// starting code for three.js in vite+vanilla+javascript environment
+
+const pane = new Pane();
+const scene = new THREE.Scene();
+
+const sphereGeometry = new THREE.sphereGeometry();
+
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  200,
+);
+camera.position.z = 10;
+
+const canvas = document.getElementById("canvas");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, Window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  rendererReference.setSize(window.innerWidth, window.innerHeight);
+});
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const rendererloop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(rendererloop);
+};
+
+rendererloop();
